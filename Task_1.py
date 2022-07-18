a925b2559c1a6ba48e8c37b98b5d3ebc19d0ae8e
MaximBabenkov <lostbass@mail.ru>
2022-07-18 07:32:03 +0300

    Task_1

---
+++ Task_1.py
@@ -0,0 +1,25 @@
+#  Напишите программу, которая принимает на вход 
+# вещественное число и показывает сумму его цифр.
+
+#  Пример:
+
+# - 6782 -> 23
+# - 0,56 -> 11
+
+def remove_comma(number: str):
+    numerals = []
+    for i in range(len(number)):
+        if number[i] != '.':
+            numerals.append(int(number[i]))
+    return numerals
+
+def find_summ(digits: list):
+    summ = 0
+    for i in range(len(digits)):
+        summ = digits[i] + summ
+    return summ
+
+numb = input('Enter your number: ')
+dig = remove_comma(numb)
+print('The sum of digits of this number:', find_summ(dig))
+
